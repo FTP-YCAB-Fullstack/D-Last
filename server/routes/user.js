397d11c97180d1318aebdb0c280b771e94350a6b
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-10-09 13:39:28 +0700

    done authorization

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 const userRoute = require("express").Router()
 const userController = require("../controllers/user")
+const authentication = require("../middlewares/authentication")
+const authorization = require("../middlewares/authorization")
 
-userRoute.get("/users",userController.getAll)
+userRoute.get("/users",authentication,authorization(["admin"]),userController.getAll)
 userRoute.post("/register",userController.regist)
 userRoute.post("/login",userController.login)
 module.exports = userRoute
