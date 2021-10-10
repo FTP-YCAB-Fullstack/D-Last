80208627984d66afc029faf36f336b9f6797378a
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-10-10 11:24:43 +0700

    adding methhod getById, updateById, DeleteById in activity, health, and story controller

--- clients/src/components/register/register.jsx
+++ clients/src/components/register/register.jsx
@@ -1,53 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import { useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Register() {
-    const history = useHistory()
-    const [nama,setNama] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+  const history = useHistory();
+  const [nama, setNama] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    // useEffect(() => {
-    //     console.log(`Nama : ${nama}`)
-    //     console.log(`Email : ${email}`)
-    //     console.log(`Password : ${password}`)
-    // },[nama,email,password])
+  // useEffect(() => {
+  //     console.log(`Nama : ${nama}`)
+  //     console.log(`Email : ${email}`)
+  //     console.log(`Password : ${password}`)
+  // },[nama,email,password])
 
-    const submited = async (e) => {
-        e.preventDefault()
-        let data = {
-            nama,
-            email,
-            password
-        }
+  const submited = async (e) => {
+    e.preventDefault();
+    let data = {
+      nama,
+      email,
+      password,
+    };
 
-        let health = await axios.post("http://localhost:5000/register",data)
+    let health = await axios.post("http://localhost:5000/register", data);
 
-        console.log(health)
-        history.push('/login')
-    }
+    console.log(health);
+    history.push("/login");
+  };
 
-    return (
+  return (
+    <div>
+      <form onSubmit={submited}>
         <div>
-            <form onSubmit={submited}>
-                <div>
-                    <label>nama</label>
-                    <input type="text" placeholder="type your name here" onChange={(e)=>setNama(e.target.value)}/>
-                </div>
-                <div>
-                    <label>email</label>
-                    <input type="email" placeholder="type your email here" onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
-                <div>
-                    <label>password</label>
-                    <input type="password" placeholder="type your password here" onChange={(e)=>setPassword(e.target.value)}/>
-                </div>
-
-                <input type="submit" value="Submit" />
-            </form>
+          <label>nama</label>
+          <input
+            type="text"
+            placeholder="type your name here"
+            onChange={(e) => setNama(e.target.value)}
+          />
         </div>
-    )
+        <div>
+          <label>email</label>
+          <input
+            type="email"
+            placeholder="type your email here"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>password</label>
+          <input
+            type="password"
+            placeholder="type your password here"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
