f7071ad362d33e78844e314876de5909ff94b964
alfathya <alfathya.kha01@gmail.com>
2021-10-10 11:30:54 +0700

    test link hospital,home,story

--- clients/src/App.js
+++ clients/src/App.js
@@ -16,7 +16,6 @@ function App() {
     <>
       <Router>
       <Switch>
-        {/* <Route path="/home" exact component={HomePage}/> */}
         <Route path="/login" exact component={LoginPage}/>
         <Route path="/register" exact component={RegisterPage}/>
         <Route path='/cerita' exact component={CeritaPage}/>
