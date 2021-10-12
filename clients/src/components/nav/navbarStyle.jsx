8fdede838cdb25fdbb3a5e7fbfc154093a3f1c16
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-10-12 12:05:08 +0700

    Home page

--- clients/src/components/nav/navbarStyle.jsx
+++ clients/src/components/nav/navbarStyle.jsx
@@ -1,89 +1,87 @@
-import styled from 'styled-components'
-import {NavLink as Link} from 'react-router-dom'
-import {FaBars} from 'react-icons/fa'
+import styled from "styled-components";
+import { NavLink as Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 export const Nav = styled.nav`
-    background: #023A42;
-    padding: 0 2rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-`
+  background: #023a42;
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
 export const NavLink = styled(Link)`
-padding: 1rem 2rem;
-cursor: pointer;
-align-items: center;
-text-align: center;
-text-decoration: none;
-color: #fff;
-transition: all 0.3s ease-in;
-font-size: 0.9rem;
-&:hover {
-  color: #73B8BE;
-}
+  padding: 1rem 2rem;
+  cursor: pointer;
+  align-items: center;
+  text-align: center;
+  text-decoration: none;
+  color: #fff;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+  &:hover {
+    color: #73b8be;
+  }
 `;
 
 export const Bars = styled(FaBars)`
-display: none;
-color: #fff;
-flex-direction: column;
+  display: none;
+  color: #fff;
+  flex-direction: column;
 
-
-@media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: flex;
     position: absolute;
     top: 1rem;
     right: 20px;
     transform: translate(-100, 75%);
     font-size: 2rem;
-    cursor: pointer
-}
-`
+    cursor: pointer;
+  }
+`;
 export const NavBtn = styled.nav`
-display: flex;
-align-items: center;
-margin-right: 24px;
-color: #fff;
-`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  color: #fff;
+`;
 export const BtnLink = styled(Link)`
-border-radius: 4px;
-background: #3A9CA3;
-padding: 5px 15px;
-margin-right: 60px;
-color: #fff;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
+  border-radius: 4px;
+  background: #3a9ca3;
+  padding: 5px 15px;
+  margin-right: 60px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
 
-
-&:hover{
+  &:hover {
     transition: all 0.2s ease-in-out;
-    background: #9EECF3;
-    color: #0110606
-}
-`
+    background: #9eecf3;
+    color: #0110606;
+  }
+`;
 
 export const BtnOut = styled.button`
-border-radius: 4px;
-background: #3A9CA3;
-padding: 5px 15px;
-margin-right: 50px;
-color: #fff;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
-margin-left : 2rem;
+  border-radius: 4px;
+  background: #3a9ca3;
+  padding: 5px 15px;
+  margin-right: 50px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 2rem;
 
-&:hover{
+  &:hover {
     transition: all 0.2s ease-in-out;
-    background: #73B8BE;
-    color: #73B8BE;
-}
-`
+    background: #73b8be;
+    color: #73b8be;
+  }
+`;
